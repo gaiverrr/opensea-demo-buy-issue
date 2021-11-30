de6206e8069f073490c199f2a01f33fbe2db22e5
Yuriy Kaygorodov <gaiverrr@gmail.com>
2021-11-30 17:19:13 +0300

    init commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type {NextPage} from 'next'
 import styles from '../../styles/Home.module.css'
 import buyToken from "../features/buyToken";
 
 const Home: NextPage = () => {
-  const buy = async () => {
-    await buyToken('4')
-  }
+    const buy = async () => {
+        await buyToken('4')
+    }
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <button onClick={() => buy()}>
-          buy
-        </button>
-      </main>
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <button onClick={() => buy()}>
+                    buy
+                </button>
+                <a href="https://testnets.opensea.io/assets/0xa480902611e4aa4b32347a853aa771da78b495f7/4"
+                   target={'_blank'} rel="noreferrer">item for purchase</a>
+            </main>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Home
